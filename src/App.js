ef735b383c6f51dc32aa6320bb47471f57a345aa
Jeyabalaji <jeyabalaji.tm@grabtaxi.com>
2023-03-14 23:09:49 +0530

    intermediate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { LodashSamples } from './LodashSamples';
+import Druid from './Druid';
 
 function App() {
   return (
     <div className="App">
       Learn React
       <LodashSamples />
+      <Druid />
     </div>
   );
 }
